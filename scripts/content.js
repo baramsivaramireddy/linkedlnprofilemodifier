6bf8165e94e9c648237ef8af31f400c32b30ddb2
baramsivaramireddy <baramsivarami@gmail.com>
2024-05-28 19:38:00 +0530

    now image is requested from pexel server - api key changed

--- scripts/content.js
+++ scripts/content.js
@@ -1,16 +1,14 @@
 
 
-const DEFAULT_PROFILE_PICTURE_FOR_ALL_USER = 'https://robohash.org/username.png'
-
+let DEFAULT_PROFILE_PICTURE_FOR_ALL_USER = 'https://robohash.org/username.png'
 
 const callback = (mutationList, observer) => {
 
     ChangeProfilePicturesOfOtherUsers();
     ChangeSmallProfilePicture();
     ChangeMyprofilePicture();
+    changeProfileInChat();
 };
-
-
 
 
 // mutationObserver for observing the dom changes
@@ -59,3 +57,40 @@ const ChangeSmallProfilePicture = () => {
         image.src = DEFAULT_PROFILE_PICTURE_FOR_ALL_USER
     })
 }
+
+const changeProfileInChat = () => {
+
+    const myProfileImages = document.querySelectorAll('.presence-entity__image');
+
+    myProfileImages.forEach((image) => {
+
+        image.src = DEFAULT_PROFILE_PICTURE_FOR_ALL_USER
+    })
+}
+
+
+const fetchTheImage = async () => {
+
+    let response = await fetch("https://api.pexels.com/v1/search?query=person&per_page=1", {
+        method: "GET",
+
+        headers: {
+            'Authorization': "YCjRfJK9rTjfsXcsUsnjn7Ap3uy7z4vZDtzw8Iacn0L4VvpVZBKePspx"
+        },
+    });
+
+    let body = await response.json()
+    return body.photos[0].src.landscape
+}
+
+
+
+const main = async () => {
+    DEFAULT_PROFILE_PICTURE_FOR_ALL_USER = await fetchTheImage()
+   
+}
+
+main().catch((err) => {
+
+    console.log(err)
+})
